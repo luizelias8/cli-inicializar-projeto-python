2d06b6ca4f1d0ca88c4b6c49461a1eecd581bc80
luizelias8 <luizelias8@gmail.com>
2024-06-23 19:05:36 -0300

    Adiciona funcionalidade de download do .gitignore

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 Este script facilita a inicialização de um projeto em Python ao automatizar algumas das etapas iniciais, incluindo:
 
-- Criação de um ambiente virtual (`.venv`)
-- Criação de um arquivo `README.md` vazio
+- Criação de um ambiente virtual (`.venv`).
+- Criação de um arquivo `README.md` vazio.
 - Criação de um arquivo `.gitignore` para projetos Python.
 - Inicialização de um repositório Git.
 
@@ -13,21 +13,26 @@ Este script facilita a inicialização de um projeto em Python ao automatizar al
 
 - **Ambiente Virtual**: Configura um ambiente virtual na pasta do projeto para gerenciar dependências.
 - **Arquivo `README.md`**: Cria um arquivo `README.md` vazio para seu projeto.
-- **Arquivo `.gitignore`**: Cria um arquivo `.gitignore` básico para Python.
+- **Arquivo `.gitignore`**: Baixa um arquivo `.gitignore` padrão para Python do GitHub ou cria um arquivo `.gitignore` básico se o download falhar.
 - **Inicialização Git**: Cria um repositório Git na pasta do projeto e faz um commit inicial.
 
 ## Pré-requisitos
 - Python 3.x instalado.
+- Acesso à internet para baixar o arquivo `.gitignore` (opcional, mas recomendado).
 
 ## Como Usar
 
 ### Instalação
 
 1. Clone este repositório para sua máquina local:
-
     ```
     git clone https://github.com/luizelias8/cli-inicializar-projeto-python.git
     cd cli-inicializar-projeto-python
+    ```
+
+2. Instale as dependências listadas no arquivo `requirements.txt` usando pip:
+    ```
+    pip install -r requirements.txt
     ```
 
 ### Execução
--- cli_inicializar_projeto_python.py
+++ cli_inicializar_projeto_python.py
@@ -1,8 +1,21 @@
 from argparse import ArgumentParser
 import os
 import subprocess
+import requests
 
-__version__ = '1.0.0'
+__version__ = '1.2.0'
+
+def download_gitignore(pasta_destino):
+    url_gitignore = 'https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore'
+    resposta = requests.get(url_gitignore)
+    if resposta.status_code == 200:
+        with open(os.path.join(pasta_destino, '.gitignore'), 'wb') as arquivo:
+            arquivo.write(resposta.content)
+        print('Arquivo .gitignore baixado e salvo com sucesso.')
+        return True
+    else:
+        print(f'Falha ao baixar o arquivo .gitignore. Código de status: {resposta.status_code}')
+        return False
 
 def criar_projeto(diretorio):
     pasta_projeto = os.path.abspath(diretorio)
@@ -12,9 +25,13 @@ def criar_projeto(diretorio):
     with open(caminho_readme, 'w') as arquivo_readme:
         pass # Cria o arquivo README.md vazio
 
-    caminho_gitignore = os.path.join(pasta_projeto, '.gitignore')
-    with open(caminho_gitignore, 'w') as arquivo_gitignore:
-        arquivo_gitignore.write('\n'.join(['.venv', '__pycache__']))
+    # Tentativa de download do .gitignore do GitHub
+    if not download_gitignore(pasta_projeto):
+        # Se o download falhar, cria um .gitignore básico manualmente
+        caminho_gitignore = os.path.join(pasta_projeto, '.gitignore')
+        with open(caminho_gitignore, 'w') as arquivo_gitignore:
+            arquivo_gitignore.write('\n'.join(['.venv', '__pycache__']))
+        print('Criado .gitignore básico manualmente')
 
     comandos = [
         [
